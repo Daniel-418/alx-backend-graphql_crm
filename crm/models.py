9f687c2d8087f58272a8d00d1aee2b0dda3c8a29
Daniel Komolafe <daniel@komolafe.dev>
2026-01-07 23:16:57 -0600

    finished task

--- crm/models.py
+++ crm/models.py
@@ -4,5 +4,8 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField()
-    email = models.CharField(max_length=100)
+    email = models.EmailField()
     phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
